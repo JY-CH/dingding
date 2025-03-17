9e6a5c6a61e86f0261fc7e801b2798168c922ec3
최재영 <horizontal4@naver.com>
2025-03-17 16:35:39 +0900

    feat: 백엔드 프로젝트 세팅

---
+++ backend/src/main/java/com/ssafy/ddingga/DdinggaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ddingga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdinggaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdinggaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/ddingga/DdinggaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ddingga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdinggaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
