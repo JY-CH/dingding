cfa80d6b914c16ddc1215d1b95f95643a96f031b
정민규 <ktfig7023@naver.com>
2025-03-25 16:35:33 +0900

    fix : 빌드 오류 수정(대소문자)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import DetailPage from './pages/DetailPage';
 import EditPage from './pages/EditPage';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
-import SearchPage from './pages/search/SearchPage';
+import SearchPage from './pages/SearchPage';
 import './index.css';
 
 function App() {
