18c53d1a16ade3dfc51efdba8a8f95ceaa9f96af
이용재 <lyj3023@naver.com>
2025-03-26 16:05:10 +0900

    feat : DashboardResponse 생성

---
+++ backend/src/main/java/com/ssafy/ddingga/facade/dashboard/dto/response/DashboardResponse.java
@@ -0,0 +1,25 @@
+package com.ssafy.ddingga.facade.dashboard.dto.response;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DashboardResponse {
+
+	private Integer userId;
+	private String username;
+	private LocalTime playtime;
+	private Integer playtimeRank;
+	private Integer totalTry;
+	private Integer totalTryRank;
+	private List<ChordScoreDto> chordScoreDtos;
+	private List<replayDto> replays;
+}
