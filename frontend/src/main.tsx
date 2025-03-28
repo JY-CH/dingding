55cf269228c6f839969659828c3aeadff9fded59
정민규 <ktfig7023@naver.com>
2025-03-28 15:02:42 +0900

    design : 파비콘 변경 및 타이틀 변경

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react';
 
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,4 +13,4 @@ createRoot(document.getElementById('root')!).render(
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
