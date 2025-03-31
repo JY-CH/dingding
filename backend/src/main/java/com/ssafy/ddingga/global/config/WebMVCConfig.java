931be7f954292287ce0aabf2c4f98a3123455e5d
이용재 <lyj3023@naver.com>
2025-03-31 12:10:32 +0900

    fix : cors, dev와 prod 분리

--- backend/src/main/java/com/ssafy/ddingga/global/config/WebMVCConfig.java
+++ backend/src/main/java/com/ssafy/ddingga/global/config/WebMVCConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.ddingga.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,20 +8,32 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
 
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins(
-				"https://j12d105.p.ssafy.io",
-				"http://j12d105.p.ssafy.io",
-				"http://localhost:8080",
-				"http://localhost:8090",
-				"http://localhost:5173"
-			)
+			.allowedOrigins(getAllowedOrigins())
 			.allowedHeaders("*")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
 			.allowCredentials(true)
 			.exposedHeaders("Authorization")
 			.maxAge(3600);
+	}
+
+	private String[] getAllowedOrigins() {
+		if ("dev".equals(activeProfile)) {
+			return new String[]{
+				"http://localhost:5173",
+				"http://localhost:8080",
+				"http://localhost:8090"
+			};
+		} else {
+			return new String[]{
+				"https://j12d105.p.ssafy.io",
+				"http://j12d105.p.ssafy.io"
+			};
+		}
 	}
 }
