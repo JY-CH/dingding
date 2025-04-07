4a545a4273504c3ed9bab1fe6018ed4826670e62
정민규 <ktfig7023@naver.com>
2025-04-07 15:18:30 +0900

    fix : url 디버깅 추가

--- frontend/src/services/guitarChordApi.ts
+++ frontend/src/services/guitarChordApi.ts
@@ -7,6 +7,7 @@ const API_URL = import.meta.env.VITE_BACKEND_URL;
 export const guitarChordApi = {
   // 템플릿 데이터 가져오기
   async getTemplateData(): Promise<TemplateDataResponse> {
+    console.log(API_URL);
     const response = await axios.get<TemplateDataResponse>(`${API_URL}/template-data`);
     console.log('템플릿 데이터:', response.data); // 디버깅용 로그 추가
     return response.data;
