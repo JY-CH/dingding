1bd1c778eeec4416b5d74fca4dff695ede65238f
임재열 <dlawoduf15@naver.com>
2025-04-08 17:21:27 +0900

    merge:merge위한 push

--- frontend/src/components/search/RelatedPosts.tsx
+++ frontend/src/components/search/RelatedPosts.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { motion } from 'framer-motion';
-import { FileText, MessageSquare, ThumbsUp, UserCircle } from 'lucide-react';
+import { MessageSquare, UserCircle } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 import { SearchCommunityPost } from '../../types/index';
